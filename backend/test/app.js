554d8c9fb31b19fc911b18cde5f5b5d7076a2c2c
Myhailo Nyskohuz <61565249+65d@users.noreply.github.com>
2024-01-12 13:52:55 +0100

    Update app.js
    Добавив базу даних

--- backend/test/app.js
+++ backend/test/app.js
@@ -1,15 +1,58 @@
+const mysql = require('mysql2');
 const express = require('express')
 
 
-const app = express() 
+const app = express()
 const port = 3000
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '20112005',
+    database: 'store_tests_DB',
+});
 
 
 app.use(express.json())
 
 
-app.get("/home", function(req,resp) {
+app.get("/home", function (req, resp) {
     resp.send("Hello world!")
+})
+
+app.get("/dbtest", function (req, resp) {
+    connection.connect((err) => {
+        if (err) {
+            console.error('Error connecting to MySQL:', err);
+            return;
+        }
+        console.log('Connected to MySQL database');
+
+
+        connection.query('SELECT * FROM products', (queryError, results) => {
+            if (queryError) {
+                console.error('Error executing query:', queryError);
+                return;
+            }
+
+
+            console.log('Retrieved data:');
+            console.log(results);
+
+            resp.send(results)
+            connection.end((endError) => {
+                if (endError) {
+                    console.error('Error closing connection:', endError);
+                    return;
+                }
+                console.log('Disconnected from MySQL database');
+            });
+        });
+    });
+
+
+
 })
 
 app.listen(port)
