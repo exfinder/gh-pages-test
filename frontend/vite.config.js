4349883bc26e81e1c0e0e3070be5bdd3d9f969ee
Vitaliy Mikhaylovskiy <Vitaliy.Mikhaylovskiy@bigchange.com>
2024-02-07 00:58:46 +0200

    fix base path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   server: {
     port: 80,
   },
-})
+  base: "/gh-pages-test/",
+});
